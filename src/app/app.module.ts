aab88e34bb5d48f2dbaf3c9156d2c5ad58808a1a
Lejito <alejocrrs@gmail.com>
2023-11-11 10:39:50 -0500

    Remove MatIcon module on app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -43,7 +42,6 @@ import { SettingsPasswordComponent } from './components/settings-password/settin
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule,
     FormsModule,
   ],
   providers: [],
