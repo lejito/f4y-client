a32a93303a0cb679c5caa32fe533cb9341472ae6
Pskdr <juanca.naranjo03@gmail.com>
2024-05-21 12:58:24 -0500

    fix: fixing problems with ts config

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,24 +1,2 @@
 import './commands';
 import 'cypress-wait-until';
-
-// Cypress Test Configuration
-Cypress.config({
-  watchForFileChanges: false,
-  videoCompression: 32,
-  videoUploadOnPasses: false,
-  video: true,
-  screenshotOnRunFailure: true,
-  trashAssetsBeforeRuns: true,
-  reporter: 'junit',
-  reporterOptions: {
-    mochaFile: 'cypress/results/output.xml',
-  },
-  env: {
-    codeCoverageTaskName: `instrumented-code-coverage`,
-  },
-  tsCompilerOptions: {
-    sourceMap: true, // <-- Remove this line or set it to false
-    inlineSourceMap: false, // <-- Remove this line or set it to false
-    types: ['cypress', 'node'],
-  },
-});
