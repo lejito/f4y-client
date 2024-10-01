5df1240eeb7e2f33ba72f122a3b9d5edd6a66c2e
Lejitop <alejocrrs@gmail.com>
2024-09-30 23:26:28 -0500

    Update environment.development.ts with local API key

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // apiKey: 'http://localhost:3000/api',
-  apiKey: 'https://f4y-server.onrender.com/api',
+  apiKey: 'http://localhost:3000/api',
 };
